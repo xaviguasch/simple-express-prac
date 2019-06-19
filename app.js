9789e20639251832bbba5587258a24de6a563bf1
xaviguasch <xaviguasch@gmail.com>
2019-06-19 10:48:04 +0200

    added Express router

--- app.js
+++ app.js
@@ -3,23 +3,15 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.get('/favicon.ico', (req, res) => res.status(204)) // boilerplate code, conflict with favicon
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  )
-})
+app.use(adminRoutes)
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from express</h1>')
-})
+app.use(shopRoutes)
 
 app.listen(4000, console.log('listening on port 4000'))
