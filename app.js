bab5d3a63f1c6411eae0297a2bff19b6ffc5fd87
xaviguasch <xaviguasch@gmail.com>
2019-06-19 10:54:22 +0200

    added a 404 error page

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.get('/favicon.ico', (req, res) => res.status(204)) // boilerplate code, conf
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(adminRoutes)
-
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(4000, console.log('listening on port 4000'))
